6f858aab8591b8a00e3bf5326318075f712cf650
Alexander Matveev <sa-matik@yandex.com>
2022-12-21 18:13:55 +0100

    Add checks for texts of answers

--- src/main/java/ru/yandex/practicum/page_objects/LandingPage.java
+++ src/main/java/ru/yandex/practicum/page_objects/LandingPage.java
@@ -12,18 +12,21 @@ public class LandingPage {
     private WebDriver driver;
 
     // список вопросов о важном
-    private By ImportantQuestionsList = By.className("accordion");
+    private By importantQuestionsList = By.className("accordion");
     // элемент списка вопросов о важном
-    private By ImportantQuestionsItems = By.className("accordion__item");
+    private By importantQuestionsItems = By.className("accordion__item");
 
     // Ответ на вопрос
-    private By Answer = By.xpath(".//div[@class='accordion__panel']");
+    private By answer = By.xpath(".//div[@class='accordion__panel']");
+
+    // Текст ответа на вопрос
+    private By answerTextRelativeLocator = By.xpath(".//p");
 
     // кнопка "Заказать" в хедере главной страницы
-    private By OrderButtonInHeader = By.xpath(".//div[contains(@class, 'Header')]//button[text() = 'Заказать']");
+    private By orderButtonInHeader = By.xpath(".//div[contains(@class, 'Header')]//button[text() = 'Заказать']");
 
     // кнопка "Заказать" на главной странице
-    private By OrderButtonOnPage = By.xpath(".//div[contains(@class, 'Home_ThirdPart')]//button[text() = 'Заказать']");
+    private By orderButtonOnPage = By.xpath(".//div[contains(@class, 'Home_ThirdPart')]//button[text() = 'Заказать']");
 
 
 
@@ -36,23 +39,28 @@ public class LandingPage {
     }
 
     public void clickImportantQuestionsItem(int index) {
-        List<WebElement> items = driver.findElements(ImportantQuestionsItems);
+        List<WebElement> items = driver.findElements(importantQuestionsItems);
         WebElement item = items.get(index);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView()", item);
         item.click();
     }
 
     public boolean isAnswerVisible(int index) {
-        String  display = driver.findElements(Answer).get(index).getCssValue("display");
+        String  display = driver.findElements(answer).get(index).getCssValue("display");
         return !display.equals("none");
     }
 
+    public String getAnswerText(int index) {
+       WebElement element = driver.findElements(answer).get(index);
+       return element.findElement(answerTextRelativeLocator).getText();
+    }
+
     public void clickOrderButtonInHeader() {
-        driver.findElement(OrderButtonInHeader).click();
+        driver.findElement(orderButtonInHeader).click();
     }
 
     public void clickOrderButtonOnPage() {
-        driver.findElement(OrderButtonOnPage).click();
+        driver.findElement(orderButtonOnPage).click();
     }
 
 }
